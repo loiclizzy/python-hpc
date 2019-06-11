29ed3f078aa693a5c72e4e3b518600ebed393ba5
Franck Thollard <franck.thollard@univ-grenoble-alpes.fr>
2019-06-11 12:21:20 +0200

    un-broking V4

--- pyfiles/dtw_cort_dist/V4_c_dtw_c_cort/dtw_cort_dist_mat.py
+++ pyfiles/dtw_cort_dist/V4_c_dtw_c_cort/dtw_cort_dist_mat.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 
-import os
-import argparse
 import numpy as np
+import sysconfig
+from functools import partial
 from cffi import FFI
+from runpy import run_path
+from pathlib import Path
+
+util = run_path(Path(__file__).absolute().parent.parent / "util.py")
 
 ffi = FFI()
-ffi.cdef("""double dtw(double* x, double* y, int size_x, int size_y);
-            double cort(double *x, double *y, int size);""")
-my_dir = os.path.dirname(os.path.realpath(__file__))
-dllib = ffi.dlopen(os.path.join(my_dir, "build", "lib.linux-x86_64-2.7", "distances.so"))
+ffi.cdef("double dtw(double* x, double* y, int size_x, int size_y);")
+ffi.cdef("double cort(double* x, double* y, int size_x);")
+my_dir = Path(__file__).absolute().parent
+dllib = ffi.dlopen(
+    str(my_dir / ("cdtw" + sysconfig.get_config_var("EXT_SUFFIX")))
+)
 
 
 def serie_pair_index_generator(number):
@@ -44,23 +50,7 @@ def cort(serie_a, serie_b):
     ret = dllib.cort(a_ptr, b_ptr, len(serie_a))
     return ret
 
-
-if __name__ == "__main__":
-    PARSER = argparse.ArgumentParser(description="Computes the distance matrix btw series")
-    PARSER.add_argument("input", type=str, help="input file ")
-    PARSER.add_argument("-n", type=int, default=None, help="number of series (default all 200)")
-    PARSER.add_argument("-v", action="store_true", default=None, help="visualize the matrix")
-
-    ARGS = PARSER.parse_args()
-
-    series = None
-    # load the input data
-    series = np.load(ARGS.input)
-
-    if ARGS.n:
-        nb_series = ARGS.n
-    else:
-        nb_series = series.shape[0]
+def compute(series, nb_series):
     gen = serie_pair_index_generator(nb_series)
 
     _dist_mat_dtw = np.zeros((nb_series, nb_series), dtype=np.float64)
@@ -69,15 +59,14 @@ if __name__ == "__main__":
         dist_dtw = cDTW(series[t1], series[t2])
         _dist_mat_dtw[t1, t2] = dist_dtw
         _dist_mat_dtw[t2, t1] = dist_dtw
-        dist_cort = 0.5*(1-cort(series[t1], series[t2]))
+        dist_cort = 0.5 * (1 - cort(series[t1], series[t2]))
         _dist_mat_cort[t1, t2] = dist_cort
         _dist_mat_cort[t2, t1] = dist_cort
 
-    if ARGS.v:
-        import matplotlib.pyplot as plt
-        fig, axes = plt.subplots(2)
-        cax0 = axes[0].imshow(_dist_mat_dtw, cmap=plt.cm.gray)
-        axes[0].set_title("dtw")
-        cax1 = axes[1].imshow(_dist_mat_cort, cmap=plt.cm.gray)
-        axes[1].set_title("cort")
-        plt.show()
+    return _dist_mat_dtw, _dist_mat_cort
+
+
+main = partial(util["main"], compute)
+
+if __name__ == "__main__":
+    main()
